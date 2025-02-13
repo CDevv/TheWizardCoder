7a9474f5b4094e32af6bc69192b03d2f4c31d161
CDevv <cdevyt@gmail.com>
2025-02-13 11:22:28 +0200

    Set Up Unit Tests
    -Set up unit tests using gdUnit4Net
    -Change target framework to .NET 8 (required by gdUnit4Net)

---
+++ scripts/tests/BattleEffectTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GdUnit4;
+using static GdUnit4.Assertions;
+
+namespace TheWizardCoder.Tests
+{
+    [TestSuite]
+    public class BattleEffectTests
+    {
+        [TestCase]
+        public void TestAsset()
+        {
+            AssertBool(true).IsTrue();
+        }
+    }
+}
